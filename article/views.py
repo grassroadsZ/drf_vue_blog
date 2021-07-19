fe5d03b1a839f5de4715473162da4dfa228a6ae0
grassroadsZ <zys17666541106@163.com>
2021-07-20 07:54:58 +0800

    使用mixins.RetrieveModelMixin,                     mixins.UpdateModelMixin,                     mixins.DestroyModelMixin,                     generics.GenericAPIView                     实现ArticleDetail视图

--- article/views.py
+++ article/views.py
@@ -1,10 +1,15 @@
+
+
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
-from rest_framework.views import APIView
+
 from rest_framework import status
+from rest_framework import mixins
+from rest_framework import generics
+
 from article.models import Article
 
-from django.http import Http404
+
 
 from article.serializers import ArticleListSerializer
 from article.serializers import ArticleDetailSerializer
@@ -26,37 +31,19 @@ def article_list(request):
 
 
 
-class ArticleDetail(APIView):
+class ArticleDetail(mixins.RetrieveModelMixin,
+                    mixins.UpdateModelMixin,
+                    mixins.DestroyModelMixin,
+                    generics.GenericAPIView):
     """文章详情视图"""
+    queryset = Article.objects.all()
+    serializer_class = ArticleDetailSerializer
 
-    def get_object(self, pk):
-        """获取单个文章对象"""
-        try:
-            # pk 即主键，默认状态下就是 id
-            return Article.objects.get(pk=pk)
-        except:
-            raise Http404
+    def get(self, request, *args, **kwargs):
+        return self.retrieve(request, *args, **kwargs)
 
-    def get(self, request, pk):
-        article = self.get_object(pk)
-        serializer = ArticleDetailSerializer(article)
-        # 返回 Json 数据
-        return Response(serializer.data)
+    def put(self, request, *args, **kwargs):
+        return self.update(request, *args, **kwargs)
 
-    def put(self, request, pk):
-        article = self.get_object(pk)
-        serializer = ArticleDetailSerializer(article, data=request.data)
-        # 验证提交的数据是否合法
-        # 不合法则返回400
-        if serializer.is_valid():
-            # 序列化器将持有的数据反序列化后，
-            # 保存到数据库中
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    def delete(self, request, pk):
-        article = self.get_object(pk)
-        article.delete()
-        # 删除成功后返回204
-        return Response(status=status.HTTP_204_NO_CONTENT)
+    def delete(self, request, *args, **kwargs):
+        return self.destroy(request, *args, **kwargs)
